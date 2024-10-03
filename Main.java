e80d801bf7245e342a82f75011e17453c0795e30
João Vitor <126514610+JoaoVACoelh@users.noreply.github.com>
2024-10-03 13:59:30 +0000

    att

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+        Pessoa pessoa = new Pessoa();
+        System.out.println(pessoa.getNome());
+    }
+}
---
+++ Pessoa.java
@@ -0,0 +1,7 @@
+public class Pessoa{
+    public String nome = "Ana";
+
+    public String getNome(){
+        return nome;
+    }
+}
