7b2b4c8579b634dfd243c1f2b2f7baff0300bbdb
João Vitor <126514610+JoaoVACoelh@users.noreply.github.com>
2024-10-03 14:32:30 +0000

    att2

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+import pessoa.Pessoa;
+import pessoa.PessoaFisica;
+
 public class Main{
     public static void main(String[] args){
         Pessoa pessoa = new Pessoa();
+        PessoaFisica pessoafisica = new PessoaFisica();
+
         System.out.println(pessoa.getNome());
+        pessoa.setNome("Beatriz");
+        System.out.println(pessoa.getNome());
+
+        pessoafisica.setNome("Rafael");
+        pessoafisica.setCpf("123.456.789-09");
+
+        System.out.println(pessoafisica.getNome());
+        System.out.println(pessoafisica.getCpf());
     }
 }
--- pessoa/Pessoa.java
+++ pessoa/Pessoa.java
@@ -1,7 +1,12 @@
+package pessoa;
+
 public class Pessoa{
     public String nome = "Ana";
 
     public String getNome(){
         return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
     }
 }
